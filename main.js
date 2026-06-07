188e4102f7243b1b34b8a4842d7f31ffcc20ba36
papadcha <papadcha@gmail.com>
2026-06-07 18:30:51 +0300

    Αφαίρεση DevTools

--- main.js
+++ main.js
@@ -143,10 +143,7 @@ function createWindow() {
   });
 
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-    mainWindow.webContents.openDevTools();
-  });
+  mainWindow.once('ready-to-show', () => mainWindow.show());
   mainWindow.on('closed', () => { mainWindow = null; });
 }
 
