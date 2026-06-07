3eb88fb169bf60f8761938ae86cad82cd5702804
papadcha <papadcha@gmail.com>
2026-06-07 14:43:32 +0300

    DevTools για debugging

--- main.js
+++ main.js
@@ -143,7 +143,10 @@ function createWindow() {
   });
 
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  mainWindow.once('ready-to-show', () => mainWindow.show());
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+    mainWindow.webContents.openDevTools();
+  });
   mainWindow.on('closed', () => { mainWindow = null; });
 }
 
